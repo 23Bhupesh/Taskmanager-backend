578665666c5ca211602196f7a30ee259101bfee1
Bhupesh Sahu <112116254+23Bhupesh@users.noreply.github.com>
2026-06-13 20:23:35 +0530

    Update CORS allowed origin for production deployment

--- src/main/java/com/bhupesh/taskmanager/config/SecurityConfig.java
+++ src/main/java/com/bhupesh/taskmanager/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "https://your-app-name.netlify.app")); // React dev server
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://taskmanager-bhupesh.netlify.app/")); // React dev server
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
